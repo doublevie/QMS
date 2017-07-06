f0b618a068db3cac21fad019865e603425114224
fares ABADOU <fares.abadou@gmail.com>
2017-07-06 17:19:11 +0100

    added theme

--- main.php
+++ main.php
@@ -6,6 +6,9 @@
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/main.css">
+    <style media="screen">
+.bord {border:1px solid rgba(0,0,0,0.1);min-height:80vh;padding:15px;}
+    </style>
     <script type="text/javascript" src="assets/js/frequency.min.js"></script>
 
 
@@ -14,8 +17,28 @@
 
 <?php include 'inc/menu.php' ?>
 
+<div class="container">
+<div class="col-sm-4 bord">
+<p>SERVER STATUS : OK</p>
+<p>NOMBRE DES GUICHETS : 6</p>
+<p>ERREURS : 0</p>
+
+</div>
+
+<div class="col-sm-8 bord" style="border-left:none;">
+
+
+</div>
 
 
 
+
+
+</div>
+
+
+
+
+<script src="assets/js/frequency.min.js" charset="utf-8"></script>
   </body>
 </html>
--- settings.php
+++ settings.php
@@ -10,10 +10,9 @@
 
 
   </head>
-  <body>
+  <body amber>
 
 <?php include 'inc/menu.php' ?>
-
 <div class="container">
 <div class="row">
 <div class="col-sm-4 bord" style='padding:0'>
@@ -25,7 +24,7 @@
   <li onclick="loadTab(4,this)"><i class="fa fa-cog"></i> FREUENCY</li>
   </ul>
 </div>
-<div class="col-sm-8 bord" style="border-left:none">
+<div class="col-sm-8 bord" style="border:none">
 <div class="settingsWell" data-tab="0">
   <div class="inner">
 <h1>GENERAL</h1>
@@ -50,9 +49,7 @@
   <td><input type="number" class="form-control" name="NOMBRE_GUICHETS" value="" onkeyup="save(this)"></td>
 </tr>
 
-
 </table>
-
   </div>
 </div>
 <div class="settingsWell" data-tab="1">
@@ -81,23 +78,29 @@
 <div class="settingsWell" data-tab="2">
   <div class="inner">
     <h1>EXTRA </h1>
-
   </div>
 </div>
 
 <div class="settingsWell" data-tab="3">
   <div class="inner">
     <h1>COMPTES </h1>
-
   </div>
 </div>
 
 <div class="settingsWell" data-tab="4">
   <div class="inner">
     <h1>FREQUENCY </h1>
-
   </div>
 </div>
+
+
+
+
+
+
+
+
+
 
 
 </div>
